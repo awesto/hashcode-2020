c1a50c8128c5126128594bd0b7411db70eb124a5
narko <6020peaks@gmail.com>
2020-02-20 20:05:45 +0100

    output writer

--- python/main.py
+++ python/main.py
@@ -3,6 +3,7 @@ import sys
 
 Library = namedtuple('Library', ['id', 'signup', 'bpd', 'books'])
 
+libraries = [Library(0, 2, 2, [0, 1, 2, 3, 4]), Library(1, 3, 1, [3, 2, 5, 0])]
 
 def parse_input_file(input_file):
     global num_libraries, num_books
@@ -24,15 +25,16 @@ def parse_input_file(input_file):
 
 def write_file(filename):
     with open(filename, 'w') as fh:
-        fh.write('{}\n'.format(len(permutation)))
-        for slide in permutation:
-            fh.write(slide.id + '\n')
+        fh.write('{}\n'.format(len(libraries)))
+        for lib in libraries:
+            fh.write(str(lib.id) + ' ')
+            fh.write(str(len(lib.books)) + '\n')
+            fh.write(' '.join(map(str, lib.books)) + '\n')
 
 
 if __name__ == '__main__':
-    print('hello')
     if len(sys.argv) < 3:
         print ("usage: ./main.py IN_FILE OUT_FILE")
         sys.exit(1)
-    parse_input_file(sys.argv[1])
+    #parse_input_file(sys.argv[1])
     write_file(sys.argv[2])
